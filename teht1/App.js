48a06ef41d18b125532b9db31fa6eea05523c1fc
oona123 <oona.saarikoski@hotmail.com>
2025-01-09 10:06:22 +0200

    valmis

---
+++ teht1/App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+   const [lower, setLower] = useState('')
+  const [upper, setUpper] = useState('')
+  const [age, setAge] = useState('')
+  
+  const calculate = () => {
+    const lowerResult = (220-age) * 0.65
+    setLower(lowerResult)
+    const upperResult = (220-age) * 0.85
+    setUpper(upperResult)
+  }
+  
+    return (
+      <View style={styles.container}>
+        <Text style ={styles.field}>Age</Text>
+         <TextInput
+        style={styles.field}
+        value={age}
+        onChangeText={text => setAge(text)}
+        keyboardType='decimal-pad'
+        />
+        <Text style={styles.field}>Limits</Text>
+        <Text style={styles.field}>{lower}-{upper}</Text>
+        <Button title='Calculate' onPress={calculate}></Button>
+      </View>
+    );
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+    marginLeft: 10
+  },
+  field: {
+    marginBottom: 10
+  }
+});
